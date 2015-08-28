576bd34dbc882b1c37f98d43f930edc05fdef054
thaos <kajhiua@hotmail.com>
2015-08-28 14:46:29 +0200

    Using formulae now for gauss_fit

--- R/model_fit.R
+++ R/model_fit.R
@@ -6,12 +6,23 @@
 #' @importFrom quantreg rq 
 #' @importFrom quantreg predict.rq 
 
+get_param <- function(par, mod, data){
+  mat <- model.matrix(mod, data=data)
+  param <- mat %*% par
+}
+
+complete_formula <- function(y, uncomplete_f){
+  stopifnot(length(uncomplete_f) == 2)
+  if(is.character(y)) 
+    return(as.formula(paste(y, "~", uncomplete_f[2])))
+  as.formula(paste(deparse(substitute(y)), "~", uncomplete_f[2]))
+}
+
+
 #' @export
-gauss_negll <- function(y, mu0, mu1, sig0, sig1, mu_var, sig_var){
-	stopifnot(length(mu_var) == length(sig_var))
-	n <- length(mu_var)
-	mu <- mu1*mu_var+mu0
-	sig2 <- sig1*sig_var + sig0
+gauss_negll <- function(y, mu, sig2){
+  n <- length(y)
+  stopifnot(length(mu) == n & length(sig2) == n)
 	negll <- -0.5*(-n*log(2*pi)-sum(log(sig2)) - sum((y-mu)^2/sig2))
 	if(is.na(negll)) negll <- 10^6
 	negll
@@ -33,24 +44,33 @@ gev_negll <- function(y, mu0, mu1, sig0, sig1, xi, mu_var, sig_var){
 }
 
 #' @export
-gauss_fit <- function(ydat, init=NULL){
-	gauss_lik <- function(y, init, mu_var, sig_var){
-		gauss_negll(y, init[1], init[2], init[3], init[4], mu_var, sig_var)
+gauss_fit <- function(y, data, mu_mod, sig2_mod, init=NULL){
+  env <- environment()
+  nb_mup <- length(attr(terms(mu_mod), "term.labels"))+attr(terms(mu_mod),"intercept")
+	gauss_lik <- function(init){
+    mu_init <- init[1:nb_mup]
+    sig2_init <- init[-(1:nb_mup)]
+    mu <- get_param(mu_init, mu_mod, data)
+    sig2 <- get_param(sig2_init, sig2_mod, data)
+		gauss_negll(y, mu, sig2) 
 	}
 	if(is.null(init)){
-		y_fit <- lm(y~mu_var, data=ydat)
-		fit_res <- residuals(y_fit)
-		var_fit <- lm(fit_res^2~sig_var, data=ydat)
+		y_fit <- lm.fit(x=model.matrix(mu_mod, data=data), y=y)
+		fit_res2 <- residuals(y_fit)^2
+		var_fit  <- lm.fit(x=model.matrix(sig2_mod, data=data), y=fit_res2)
 		init <- c(coefficients(y_fit), coefficients(var_fit))
 		print("--- Parameters Initialization -----")
-		print(paste("neg-likelihood =", gauss_lik(y=ydat$y, init=init, mu_var=ydat$mu_var, sig_var=ydat$sig_var)))
+    print(paste("neg-likelihood =", gauss_lik(init)))
 		print(paste("mle =", do.call(paste, as.list(init))))
 	}
-	y_fit <- nlminb(start=init, gauss_lik, y=ydat$y, mu_var=ydat$mu_var, sig_var=ydat$sig_var)
+	y_fit <- nlminb(start=init, gauss_lik)
 	print("--- Parameters Optimization -----")
 	print(paste("neg-likelihood =", y_fit$objective))
 	print(paste("mle =", do.call(paste, as.list(y_fit$par))))
-	y_fit$ydat <- ydat
+  y_fit$y <- y
+	y_fit$data <- data
+	y_fit$mu_mod <- mu_mod
+	y_fit$sig2_mod <- sig2_mod
 	attr(y_fit, "class")= "gauss_fit"
 	y_fit
 }
@@ -137,24 +157,34 @@ plot.gpd_fit <- function(x, ...){
   plot(res)
 }
 
+#' @export
+compute_par.gauss_fit <- function(object, newdata, ...){
+  mu_mod <- object$mu_mod
+  sig2_mod <- object$sig2_mod
+  nb_mup <- length(attr(terms(mu_mod), "term.labels"))+attr(terms(mu_mod),"intercept")
+  nb_sig2p <- length(attr(terms(sig2_mod), "term.labels"))+attr(terms(sig2_mod),"intercept")
+  mu_par <- object$par[1:nb_mup]
+  sig2_par <- object$par[-(1:nb_mup)]
+  data.frame("mu"=get_param(mu_par, mu_mod, newdata), "sig2"=get_param(sig2_par, sig2_mod, newdata))
+}
 
 #' @export
 plot.gauss_fit <- function(x, ...){
-	param <- x$par
-	pred <- param[1] + param[2] * x$ydat$mu_var
-	res <- x$ydat$y - pred
-	var_res <- param[3] +param[4] * x$ydat$sig_var
-	res_std <- res / sqrt(var_res)
+	param <- compute_par.gauss_fit(x, x$data)
+	mu <- param$mu
+	sig2 <- param$sig2
+	res <- x$y - mu
+	res_std <- res / sqrt(sig2)
 	par(mfrow=c(2,2))
-	plot(pred, res_std, main="Standardized Residuals vs Fitted", xlab="Fitted", ylab="Standardized Residuals")
+	plot(mu, res_std, main="Standardized Residuals vs Fitted", xlab="Fitted", ylab="Standardized Residuals")
 	abline(h=0, ,col="blue")
 	extRemes::qqnorm(res_std)
 	abline(a=0, b=1, col="blue")
 	plot(dens <- density(res_std), main="Transformed Data")
 	lines(dens$x, dnorm(x=dens$x), col="blue", lty=2)
-	plot(x$ydat$y, type="l")
+	plot(x$y, type="l")
 	p=1-c(0.5, 0.05, 0.01)
-	qXX=mapply(qnorm, mean=pred, sd=sqrt(var_res), MoreArgs=list(p=p))
+	qXX=mapply(qnorm, mean=mu, sd=sqrt(sig2), MoreArgs=list(p=p))
 	for(i in seq_along(p)){
 		    lines(qXX[i,], col=i+1)
 	}
