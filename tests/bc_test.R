97724b9fc5b70a3ed6437fa5cfe41fce366fb23b
thaos <kajhiua@hotmail.com>
2015-09-10 17:05:13 +0200

    update test

--- tests/bc_test.R
+++ tests/bc_test.R
@@ -1,31 +1,41 @@
-library(MASS)
-library(extRemes)
 library(devtools)
 devtools::load_all("..") 
+
+set_fparam <- function(f, gfit){
+  l <- formals(f)
+  for( p in names(l)){
+    #     print('-------------------') 
+    #     print(p)
+    #     print(gfit[[p]] )
+    l[[p]] <- gfit[[p]]
+  }
+  l
+}
 
 n <- 350
 t <- seq(1,n)
 year <- 1850 + t - 1
 mu <- (t>100)*.01*(t-100)+100
 sigma2 <- (t>100)*.01*(t-100)+1
-sigma2 <- 1
+# sigma2 <- 1
 y <- mapply(rnorm,1,mu,sqrt(sigma2))
 covariate <- c(scale(mu))
 ydat <- data.frame(y, year, covariate)
 xp <- 102 
 t0 <- 1870
 t1 <- 2003
-pnt1 <- set_pnt(t1, xp, ydat)
-pnt0 <- set_pnt(t0, xp, ydat)
+pnt1 <- set_pnt(t1, xp, time_var="year", ydat)
+pnt0 <- set_pnt(t0, xp, time_var="year", ydat)
 
-y_bc <- bc_fit(l_lambda=seq(-10,10,0.1), y, data=ydat, mu_mod=~covariate, sig2_mod=~covariate, to_plot=TRUE)
+y_bc <- bc_fit(l_lambda=seq(-10,10,0.1), y, data=ydat, mu_mod=~covariate, sig2_mod=~covariate, time_var="year", to_plot=TRUE)
 lambda <- y_bc$lambda
 y_std <- y_bc$y_std
-tc=tcplot(y_std,c(quantile(y_std,0.5),max(y_std)))
+tc <- tcplot(y_std,c(quantile(y_std,0.5),max(y_std)))
 # Easton avec thy_bchold fixe 
 threshold <-  select_thresh(tc) 
 rate=mean(y_std >= threshold)
-y_bc_fit <- gpd_fit(y_bc$y_std,  ydat, qthreshold=1-rate)
+y_bc_fit <- gpd_fit(y_bc$y_std,  ydat, time_var="year", qthreshold=1-rate)
+y_bc_fit <- gpd_fit(y_bc$y_std,  ydat, mu_mod=~y, time_var="year", qthreshold=1-rate)
 pnt1_bc <- transform_pnt(y_bc, pnt1)
 pnt0_bc <- transform_pnt(y_bc, pnt0)
 get_far(y_bc_fit, pnt0_bc, pnt1_bc)
@@ -39,26 +49,35 @@ get_far.bc_fit <- function(y_trans, y_fit, pnt0, pnt1, qthreshold=NULL){
 }
 get_far.bc_fit(y_bc, y_bc_fit, pnt0, pnt1)
 
-get_far.bc_fit <- function(y_fit, pnt0, pnt1, qthreshold=NULL){
-  lambda <- y_fit$lambda
-  y_std <- y_fit$y_std
-  if(is.null(qthreshold)){
-    tc <- tcplot(y_std)
-    threshold <- select_thresh(tc)
-    qthreshold <- mean(y_std < threshold)
-    print(qthreshold)
-  }
-  # we suppose the boxcox transform made the time series stationnary
-  y_std_fit <- gpd_fit(res$y_std,  ydat, qthreshold=qthreshold)
-  plot(y_std_fit)
-  pnt1_bc <- transform_pnt(y_fit, pnt1)
-  pnt0_bc <- transform_pnt(y_fit, pnt0)
-  far <- get_far(y_std_fit, pnt0_bc, pnt1_bc)
-  attr(far, "gpd_fit") <- y_std_fit
-  far
+boot_func.bc_fit <- function(y_trans, y_fit, indices, pnt0, pnt1){
+  print(indices)
+  y <- y_trans$y[indices]
+  data <- y_trans$data[indices,]
+  y_bc <- bc_fit(y_trans$l_lambda, y, data, mu_mod=y_trans$mu_mod, sig2_mod=y_trans$sig2_mod, time_var=y_trans$time_var)
+  fit_method <- eval(parse(text=class(y_fit))) 
+  fit_inputs <- as.list(set_fparam(fit_method, y_fit)) 
+  fit_inputs$y <- y_bc$y_std 
+  fit_inputs$data <- data 
+  y_bc_fit  <- do.call(fit_method, fit_inputs)
+  get_far.bc_fit(y_bc, y_bc_fit, pnt0, pnt1)
 }
-far_bc <- get_far.bc_fit(fit_bc, pnt0, pnt1)
-far_bc <- get_far.bc_fit(fit_bc, pnt0, pnt1, qthreshold=.75)
+bf_bc <- boot_func.bc_fit(y_bc, y_bc_fit, indices=1:length(y), pnt0, pnt1)
+sb_bc <- simple_boot(y_bc_fit, boot_func.bc_fit, y_trans=y_bc, pnt0=pnt0, pnt1=pnt1)
+
+boot_ic.bc_fit <- function(xp, t0, t1, y_trans, y_fit, ci_p=0.95, ...){
+  pnt0 <- set_pnt(t0, xp, y_fit$time_var, y_fit$data)
+  pnt1 <- set_pnt(t1, xp, y_fit$time_var, y_fit$data)
+  far_mle <- get_far.bc_fit(y_trans, y_fit, pnt0, pnt1)
+  boot_res <- simple_boot(y_fit, boot_func.bc_fit, y_trans=y_trans, pnt0=pnt0, pnt1=pnt1)
+  alpha <- 1-ci_p
+  far_boot <- boot_res["FAR", ]
+  ic_boot <- quantile(far_boot, probs=c(alpha/2, .5, 1-alpha/2))
+  ans <- c(ic_boot, far_mle)
+  names(ans)[1:3] <- c("IC_inf", "FAR_B", "IC_sup")
+  ans
+}
+bic_bc <- boot_ic.bc_fit(xp, t0, t1, y_bc,  y_bc_fit)
+
 
 boot_bc <- function(data, indices, y, pnt0, pnt1, qthreshold){
   data_b <- data[indices,]
@@ -78,11 +97,10 @@ threshold <-  select_thresh(tc)
 rate=mean(y_std$y_std >= threshold)
 pnt1_std <- transform_pnt(y_std, pnt1)
 pnt0_std <- transform_pnt(y_std, pnt0)
-y_std_fit <- gpd_fit(y_std$y_std ,  ydat, qthreshold=1-rate)
+y_std_fit <- gpd_fit(y_std$y_std ,  ydat, time_var="year", qthreshold=1-rate)
 get_far(y_std_fit, pnt0_std, pnt1_std)
 
 get_far.std <- function(y_trans, y_fit, pnt0, pnt1, qthreshold=NULL){
-  print(deparse(sys.call()))
   pnt1_std <- transform_pnt(y_trans, pnt1)
   pnt0_std <- transform_pnt(y_trans, pnt0)
   far <- get_far(y_fit, pnt0_std, pnt1_std)
@@ -91,7 +109,7 @@ get_far.std <- function(y_trans, y_fit, pnt0, pnt1, qthreshold=NULL){
 get_far.std(y_std, y_std_fit, pnt0, pnt1)
 
 boot_func.std <- function(y_trans, y_fit, indices, pnt0, pnt1){
-  print(indices)
+  #   print(indices)
   y <- y_trans$y[indices]
   data <- y_trans$data[indices,]
   y_std <- standardize(y, data, mu_mod=y_trans$mu_mod, sig2_mod=y_trans$sig2_mod )
@@ -106,10 +124,10 @@ bf.std <- boot_func.std(y_std, y_std_fit, indices=1:length(y), pnt0, pnt1)
 sb_std <- simple_boot(y_std_fit, boot_func.std, y_trans=y_std, pnt0=pnt0, pnt1=pnt1)
 
 boot_ic.std <- function(xp, t0, t1, y_trans, y_fit, ci_p=0.95, ...){
-  pnt0 <- set_pnt(t0, xp, y_fit$data)
-  pnt1 <- set_pnt(t1, xp, y_fit$data)
+  pnt0 <- set_pnt(t0, xp, y_fit$time_var, y_fit$data)
+  pnt1 <- set_pnt(t1, xp, y_fit$time_var, y_fit$data)
   far_mle <- get_far.std(y_trans, y_fit, pnt0, pnt1)
-  boot_res <- simple_boot(y_std_fit, boot_func.std, y_trans=y_std, pnt0=pnt0, pnt1=pnt1)
+  boot_res <- simple_boot(y_fit, boot_func.std, y_trans=y_trans, pnt0=pnt0, pnt1=pnt1)
   alpha <- 1-ci_p
   far_boot <- boot_res["FAR", ]
   ic_boot <- quantile(far_boot, probs=c(alpha/2, .5, 1-alpha/2))
@@ -117,18 +135,10 @@ boot_ic.std <- function(xp, t0, t1, y_trans, y_fit, ci_p=0.95, ...){
   names(ans)[1:3] <- c("IC_inf", "FAR_B", "IC_sup")
   ans
 }
-bic_std <- boot_ic.std(xp, t0, t1, y_std,  y_std_fit)
+print(system.time({
+  bic_std <- boot_ic.std(xp, t0, t1, y_std,  y_std_fit)
+}))
 
-set_fparam <- function(f, gfit){
-  l <- formals(f)
-  for( p in names(l)){
-    #     print('-------------------') 
-    #     print(p)
-    #     print(gfit[[p]] )
-    l[[p]] <- gfit[[p]]
-  }
-  l
-}
 
 boot_std <- function(data, indices, y, pnt0, pnt1, qthreshold){
   data_b <- data[indices,]
